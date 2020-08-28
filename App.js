bbb2e8b71d7cf68d08c11de559ff9dce9f357874
Aleksei Belinskiy <escalion86@gmail.com>
2020-08-28 18:03:02 +0700

    Added Remove function and some updates

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Navbar } from './src/Navbar';
 import { AddTodo } from './src/AddTodo';
-import { Todo } from './src/todo';
+import { Todo } from './src/Todo';
 
 export default function App() {
   const [todos, setTodos] = useState([]);
@@ -17,16 +17,20 @@ export default function App() {
       }])
   }
 
+  const removeTodo = (id) => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  }
+
   return (
     <View>
       <Navbar title='Todo App' />
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
-        <View>
-          { todos.map(todo => (
-            <Todo key={todo.id} todo={todo} />
-          ))}
-        </View>
+        <FlatList 
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({item}) => <Todo todo={item} onRemove={removeTodo} />}  
+        />
       </View>
     </View>
   );
