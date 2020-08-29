b1add181341638e08cc2981f0912019b1cc06746
Aleksei Belinskiy <escalion86@gmail.com>
2020-08-29 13:32:01 +0700

    Added EditModal

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 import { THEME } from '../theme'
 import { AppCard } from '../components/ui/AppCard'
+import { EditModal } from '../components/EditModal'
 
 export const TodoScreen = ({ goBack, todo, onRemove }) => {
+  const [modal, setModal] = useState(false)
+
   return (
     <View>
-      
+      <EditModal visible={modal} onCancel={() => setModal(false)} />
+
       <AppCard style={styles.card}>
         <Text style={styles.title}>{todo.title}</Text>
-        <Button title='Редактировать' />
+        <Button title="Редактировать" onPress={() => setModal(true)} />
       </AppCard>
-
 
       <View style={styles.buttons}>
         <View style={styles.button}>
@@ -38,10 +41,10 @@ const styles = StyleSheet.create({
     width: '45%',
   },
   title: {
-    fontSize: 20
+    fontSize: 20,
   },
   card: {
     marginBottom: 20,
-    padding: 15
-  }
+    padding: 15,
+  },
 })
